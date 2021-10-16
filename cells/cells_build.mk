a468e03778cc0763dd738b7b6dc7dcf4a26cded8
jianglin <61100700+jianglin-code@users.noreply.github.com>
2021-10-16 13:10:03 +0800

    Update cells_build.mk

--- cells/cells_build.mk
+++ cells/cells_build.mk
@@ -1,13 +1,2 @@
 
-##########################################################
-# Library for Dalvik-specific functions
-##########################################################
-ifeq (1,$(strip $(shell expr $(PLATFORM_SDK_VERSION) \< 21)))
-PRODUCT_PACKAGES += libxposed_dalvik
-endif
-
-##########################################################
-# Library for ART-specific functions
-##########################################################
-
 PRODUCT_PACKAGES += busybox celld cellc cellsservice switchsystem cellssync cellsupdownvm cswitch
